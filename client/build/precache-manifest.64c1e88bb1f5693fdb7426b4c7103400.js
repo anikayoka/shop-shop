c1537ad36b4ab2ae8f9360c6024d096d6c222855
sunchaser <anika.mclean@gmail.com>
2022-07-14 19:17:44 -0400

    lesson 2

---
+++ client/build/precache-manifest.64c1e88bb1f5693fdb7426b4c7103400.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b32e3506ad133921300b6b708f44084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d193a56adde343d4a2ec",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "1f7b4fbde98b6242bd87",
+    "url": "/static/js/2.7f18ecf0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7f18ecf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d193a56adde343d4a2ec",
+    "url": "/static/js/main.f90f8d57.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.f90f8d57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
