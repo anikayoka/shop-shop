5810891b75ab2365300a60b0233413558a32a273
sunchaser <anika.mclean@gmail.com>
2022-07-15 17:46:26 -0400

    idb

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -4,7 +4,6 @@ import { pluralize } from "../../utils/helpers"
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
-
 function ProductItem(item) {
   const [state, dispatch] = useStoreContext();
 
